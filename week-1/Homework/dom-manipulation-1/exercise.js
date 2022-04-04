b551cd2cf6e0a7e6af7ee3ed704384b653402d27
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-04-04 16:17:00 +0200

    week1 class exercise completed, homework exercise 1-4

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -4,17 +4,22 @@ Task 1
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
+    */
+let allParagraphs = document.querySelectorAll('p');
 
-    2. the first div element node
-    --> should log the ".site-header" node
+    /*2. the first div element node
+    --> should log the ".site-header" node */
 
-    3. the element with id "jumbotron-text"
-    --> should log the "#jumbotron-text" node
+let firstDiv = document.querySelector('div')
 
-    4. all the "p" elements of contained inside  the .primary-content element node
-    --> should log a list of nodes with a length of 3
+   /* 3. the element with id "jumbotron-text"
+    --> should log the "#jumbotron-text" node*/
 
-*/
+let jumboText = document.querySelector('#jumbotron-text')
+
+   /* 4. all the "p" elements of contained inside  the .primary-content element node
+    --> should log a list of nodes with a length of 3 */
+let primaryParagraph = document.querySelectorAll ('.primary-content, p')
 
 
 /*
@@ -23,7 +28,11 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+document.querySelector('#alertBtn').addEventListener('click', alertThank)
 
+function alertThank(){
+alert("Thanks for visiting Bikes for Refugees!");
+}
 
 /*
 Task 3
@@ -31,7 +40,7 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+document.querySelector('#bgrChangeBtn').addEventListener('click', function onClick (event) {document.body.style.backgroundColor ="#89CFF0"});
 
 /*
 Task 4
@@ -41,6 +50,12 @@ When a user clicks the ‘Add some text’ button, a new paragraph should be add
 */
 
 
+document.querySelector('#addTextBtn').addEventListener('click', () => {
+    const jumbotron = document.querySelector('#jumbotron')
+    const newParagraph = document.createElement("p")
+    newParagraph.innerText="LEARN MORE!"
+    return jumbotron.appendChild(newParagraph)
+})
 
 /*
 Task 5
