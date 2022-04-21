ea66c44f15cd65f3eb5e1ee4ba446579c84aa106
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-04-21 06:55:37 +0200

    Homework A-1 Successfully resolved

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -3,9 +3,16 @@
 EXERCISE 1
 
 Task 1
-Using setTimeout, change the background colour of the page after 5 seconds (5000 milliseconds). Hint: try searching for setTimeout. (https://www.w3schools.com/jsref/met_win_settimeout.asp)
+Using setTimeout, change the background colour of the page after 5 seconds (5000 milliseconds). Hint: try searching for setTimeout. (https://www.w3schools.com/jsref/met_win_settimeout.asp)*/
 
-Task 2
+const colorTimeout = setTimeout(changeBg, 5000);
+
+function changeBg() {
+    const bgCol = document.querySelector("body")
+    bgCol.style.backgroundColor = "#c4c8c5"
+}
+
+/*Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
